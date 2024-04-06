caf51ebecac4d3cce4507aca44041c04932edfcb
Johan-Mi <johanmi@protonmail.com>
2024-04-06 20:54:50 +0200

    Show error message instead of crashing

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@
 mod dom;
 
 use gtk::{
-    glib::clone, prelude::*, Application, ApplicationWindow, Entry,
+    glib::clone, prelude::*, Application, ApplicationWindow, Entry, InfoBar,
     ScrolledWindow,
 };
 use html5ever::tendril::{ByteTendril, TendrilSink};
-use std::{rc::Rc, sync::mpsc};
+use std::{error::Error, rc::Rc, sync::mpsc};
 
 fn main() -> gtk::glib::ExitCode {
     let app = Application::builder()
@@ -23,13 +23,24 @@ fn main() -> gtk::glib::ExitCode {
 fn activate(app: &Application) {
     let view = Rc::new(ScrolledWindow::builder().expand(true).build());
 
+    let info_bar = Rc::new(
+        InfoBar::builder()
+            .message_type(gtk::MessageType::Error)
+            .build(),
+    );
+
     let url_bar = Entry::new();
     url_bar.connect_key_press_event(
-        clone!(@strong view => move |url_bar, event| {
+        clone!(@strong info_bar, @strong view => move |url_bar, event| {
             if event.keyval().to_unicode() != Some('\r') {
                 return gtk::glib::Propagation::Proceed;
             }
-            open(&url_bar.text(), &view);
+            if let Err(err) = open(&url_bar.text(), &view) {
+                info_bar.set_child(Some(
+                    &gtk::Label::new(Some(&err.to_string()))
+                ));
+                info_bar.show_all();
+            }
             gtk::glib::Propagation::Stop
         }),
     );
@@ -40,32 +51,36 @@ fn activate(app: &Application) {
         .default_height(200)
         .title("Hello, World!")
         .child(
-            &gtk::Box::builder()
+            &gtk::Grid::builder()
                 .orientation(gtk::Orientation::Vertical)
                 .child(&url_bar)
                 .child(&*view)
+                .child(&*info_bar)
                 .build(),
         )
         .build();
 
     win.show_all();
+    info_bar.hide();
 }
 
-fn open(url: &str, view: &ScrolledWindow) {
+fn open(url: &str, view: &ScrolledWindow) -> Result<(), Box<dyn Error>> {
     let parts = mpsc::channel::<Box<[u8]>>();
 
     std::thread::scope(|scope| {
-        scope.spawn(|| {
-            let mut easy = curl::easy::Easy::new();
-            easy.url(url).unwrap();
-            easy.write_function(move |bytes| {
-                parts.0.send(bytes.into()).unwrap();
-                Ok(bytes.len())
+        scope
+            .spawn(|| {
+                let mut easy = curl::easy::Easy::new();
+                easy.url(url)?;
+                easy.write_function(move |bytes| {
+                    parts.0.send(bytes.into()).unwrap();
+                    Ok(bytes.len())
+                })?;
+                easy.perform()
             })
-            .unwrap();
-            easy.perform().unwrap();
-        });
-    });
+            .join()
+    })
+    .unwrap()?;
 
     let document = html5ever::parse_document(
         dom::Sink::new(),
@@ -77,4 +92,6 @@ fn open(url: &str, view: &ScrolledWindow) {
     let content = document.render();
     view.set_child(Some(&content));
     content.show_all();
+
+    Ok(())
 }
